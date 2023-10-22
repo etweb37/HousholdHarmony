cf1c512c816223dba406f773c3d8913f6032a53f
Erick Tengelitsch <etweb3@gmail.com>
2023-10-22 15:14:36 -0400

    adding new app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="center_all">
+        <span>a note</span>
+      </div>
+  );
+}
+
+export default App;
