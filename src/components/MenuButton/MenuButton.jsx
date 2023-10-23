f1685879969a92f7278e53dc34df0b5482884bb9
Erick Tengelitsch <etweb3@gmail.com>
2023-10-22 23:25:48 -0400

    Imported from Quest

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -64,7 +64,7 @@ function MenuButton(props) {
   return (
     <MenuButton1 className={props.className}>
       <Background>
-        <ButtonText>{'Menu Button Title'}</ButtonText>
+        <ButtonText>{`Button Text`}</ButtonText>
       </Background>
     </MenuButton1>
   );
