ea29be6ceab1ea3a37a866f56f2d97e2f3f5fc05
Erick Tengelitsch <etweb3@gmail.com>
2023-10-22 22:35:21 -0400

    Imported from Quest

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import questTheme from 'src/QuestUiKitLightTheme';
+import Landscape from 'src/components/Landscape/Landscape';
 
 function App() {
   return (
     <HelmetProvider>
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={questTheme}>
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Landscape />} />
+          </Routes>
         </ThemeProvider>
       </StyledEngineProvider>
     </HelmetProvider>
